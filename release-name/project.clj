6954fb10a851568b26d8e68be4faefba7f6c1ce9
Sian Davidson <sian.davidson@raisingit.com>
2018-02-22 18:07:29 +0000

    Copy the code across to git project

--- release-name/project.clj
+++ release-name/project.clj
@@ -1,12 +1,14 @@
 (defproject release-name "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Little app to generate a release name"
+  :url ""
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+    [compojure "1.5.1"]
+    [ring/ring-defaults "0.2.1"]
+    [clj-http "3.7.0"]
+    [cheshire "5.8.0"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler release-name.handler/app}
+  :ring {:handler slack-releasename.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+    {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+      [ring/ring-mock "0.3.0"]]}})
--- release-name/src/release_name/handler.clj
+++ release-name/src/release_name/handler.clj
@@ -1,11 +1,110 @@
 (ns release-name.handler
   (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+    [compojure.route :as route]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    [clj-http.client :as client]
+    [cheshire.core :refer :all]))
+
+(defn getDomain
+  [domains]
+  (str (get domains (rand-int (alength (to-array domains)))))
+)
+
+(defn domain
+  []
+  (getDomain
+    (vector "mammal" "bird" "fish" "reptile"))
+)
+
+(defn getTotalItems
+  [totalReturned]
+  (def maxItems 5000)
+
+  (if (> totalReturned maxItems)
+    maxItems
+    totalReturned)
+)
+
+(defn getSingleAnimal
+  [url]
+  (def animalsApi (
+      client/get url{
+        :headers {
+          :app_id "4503f41d", 
+          :app_key "25d720367050dc18e633a2a8d6a6496c"
+        }
+        :accept :json
+      }))
+    
+  (def animalResults (parse-string (get-in animalsApi [:body])))
+  (def totalAnimals (getTotalItems (get-in animalResults ["metadata" "total"])))
+  (def animalIndex (rand-int totalAnimals))
+  (def animals (get-in animalResults ["results"]))
+  (def singleAnimal (get-in animals [animalIndex "word"]))
+
+  (if (= singleAnimal nil)
+    (do (println animalResults)
+      (println totalAnimals)
+      (println animalIndex)
+      (println singleAnimal)
+      ))
+  (str singleAnimal)
+)
+
+(defn getSingleAdjective
+  [url]
+  (def adjectivesApi (
+      client/get url{
+        :headers {
+          :app_id "4503f41d", 
+          :app_key "25d720367050dc18e633a2a8d6a6496c"
+        }
+        :accept :json
+      }))
+  (def adjectiveResults (parse-string (get-in adjectivesApi [:body])))
+  (def totalAdjectives (getTotalItems (get-in adjectiveResults ["metadata" "total"])))
+  (def adjectiveIndex (rand-int totalAdjectives))
+  (def adjectives (get-in adjectiveResults ["results"]))
+  (def singleAdjective (get-in adjectives [adjectiveIndex "word"]))
+
+  (if (= singleAdjective nil)
+    (do (println (str "Results " adjectiveResults))
+      (println (str "Total " totalAdjectives))
+      (println (str "Index " adjectiveIndex))
+      (println (str "Single " singleAdjective))
+      ))
+  (str singleAdjective)
+)
+
+(defn getAdjective
+  [baseUrl releaseAnimal]
+  (def firstLetter (subs releaseAnimal 0 1))
+  (def url (str baseUrl "adjective?prefix=" firstLetter))
+  (str (getSingleAdjective url))
+)
+
+(def hook-url "https://impactful.slack.com/")
+(def auth-token "mkLEsf6TLuPqwaYrfcG6cZQT")
+
+(defn generate-release-name
+  []
+  (def baseUrl "https://od-api.oxforddictionaries.com/api/v1/wordlist/en/lexicalCategory=")
+  (def domainType (domain))
+  (def url (str baseUrl "noun;domains=" domainType))
+  (def releaseAnimal (getSingleAnimal url))
+  (def releaseAdjective (getAdjective baseUrl releaseAnimal))
+
+  (generate-string {:text (str releaseAdjective " " releaseAnimal)})
+)
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (POST "/slack" [] 
+    (generate-release-name))
+  (route/resources "/")
+  (route/not-found "Not Found")
+)
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes 
+   (assoc-in site-defaults [:security :anti-forgery] false))
+)
